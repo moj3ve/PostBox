8fa709ba0249024997a889ce3a8b8d0d88765654
b0kch01 <nath49ers@gmail.com>
2020-06-03 15:22:40 -0700

    Custom Classes

---
+++ Tweak Explorer/Custom Classes/Repo.swift
@@ -0,0 +1,20 @@
+//
+//  Repo.swift
+//  Tweak Explorer
+//
+//  Created by Nathan Choi on 6/3/20.
+//  Copyright © 2020 icycabbage. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class Repo: Identifiable {
+    public var id: String
+    public var name: String
+    public var url: String
+    
+    init (_ ) {
+        
+    }
+}
