ba2a4a9caf46608e066afe81f86b7d3cf450ac21
Polarizz <polarizz39@gmail.com>
2020-06-03 13:33:29 -0700

    Repos.swift works, RepoView cannot work without Repo.swift class

---
+++ Tweak Explorer/Custom Classes/Repo.swift
@@ -0,0 +1,24 @@
+//
+//  Repo.swift
+//  Tweak Explorer
+//
+//  Created by Paul Wong on 6/3/20.
+//  Copyright © 2020 icycabbage. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class Repo: Identifiable, Comparable {
+    public var repoprompt: "ADD REPO"
+    public var repoid: String
+    public var repourl: String
+
+    init(repoid: String? = nil, repourl: String? = nil) {
+        self.repoid =
+    }
+
+
+
+
+}
